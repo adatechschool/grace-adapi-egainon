7ff8e55d79e4795a80f46714c7480ad153c5084c
egainon <miliegainon@hotmail.fr>
2025-12-16 15:13:09 +0100

    skills ok

--- src/server.js
+++ src/server.js
@@ -69,7 +69,7 @@ console.log(":rocket: Serveur lancé : http://localhost:3000");
 });
 
 //- [ ] Créer une route `POST` pour les **thèmes**
-app.post("/posthemes", async function (req, res) {
+app.post("/post/themes", async function (req, res) {
 await pool.query("INSERT INTO themes (id, name, description) VALUES (5,'API REST','interface de communication entre applications') ");
 res.json( "message: thème ajouté");});
 
@@ -78,7 +78,7 @@ console.log(":rocket: Serveur lancé : http://localhost:3000");
 });
 
 // - [ ] Créer une route `PUT` pour les **thèmes**
-app.put("/puthemes", async function (req, res) {
+app.put("/put/themes", async function (req, res) {
 await pool.query("UPDATE themes SET description = 'interface de communication entre applis' WHERE id = 5");
 res.json( "message: thème changé");});
 
@@ -87,7 +87,7 @@ console.log(":rocket: Serveur lancé : http://localhost:3000");
 });
 
 //- [ ] Créer une route `DELETE` pour les **thèmes**
-app.delete("/deletethemes", async function (req, res) {
+app.delete("/delete/themes", async function (req, res) {
 await pool.query("DELETE FROM themes WHERE id = 5");
 res.json( "message: thème supprimé");});
 
@@ -97,7 +97,54 @@ console.log(":rocket: Serveur lancé : http://localhost:3000");
 
 //SKILLS
 // - [ ] Créer une route `GET` pour les **skills**
-// - [ ] Créer une route `GET` pour une **skills**
+app.get("/skills", async function (req, res) {
+const { rows } = await pool.query("SELECT * FROM skills");
+res.json(rows);});
+
+app.listen(3000, () => {
+console.log(":rocket: Serveur lancé : http://localhost:3000");
+});
+
+// - [ ] Créer une route `GET` pour une **skill**
+app.get("/skill", async function (req, res) {
+const { rows } = await pool.query("SELECT * FROM skills WHERE id = 3");
+res.json(rows);});
+
+app.listen(3000, () => {
+console.log(":rocket: Serveur lancé : http://localhost:3000");
+});
+
 // - [ ] Créer une route `POST` pour les **skills**
+app.post("/post/skills", async function (req, res) {
+await pool.query("INSERT INTO skills (id, name) VALUES (7,'Express') ");
+res.json( "message: skills ajouté");});
+
+app.listen(3000, () => {
+console.log(":rocket: Serveur lancé : http://localhost:3000");
+});
+
 // - [ ] Créer une route `PUT` pour les **skills**
+app.put("/put/skills", async function (req, res) {
+await pool.query("UPDATE skills SET name = 'Express.js' WHERE id = 7");
+res.json( "message: skill changé");});
+
+app.listen(3000, () => {
+console.log(":rocket: Serveur lancé : http://localhost:3000");
+});
+
 // - [ ] Créer une route `DELETE` pour les **skills**
+app.delete("/delete/skills", async function (req, res) {
+await pool.query("DELETE FROM skills WHERE id = 7");
+res.json( "message: skill supprimé");});
+
+app.listen(3000, () => {
+console.log(":rocket: Serveur lancé : http://localhost:3000");
+});
+
+
+//RESSOURCES_SKILLS
+// - [ ] Créer une route `GET` pour les **ressources**
+// - [ ] Créer une route `GET` pour une **ressource**
+// - [ ] Créer une route `POST` pour les **ressources**
+// - [ ] Créer une route `PUT` pour les **ressources**
+// - [ ] Créer une route `DELETE` pour les **ressources**
